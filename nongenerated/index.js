ad586ba354604783a1e7e40f530375d9baa0cf50
spandan-mozumder <mozumder.spandan.professional@gmail.com>
2025-05-10 23:33:12 +0530

    nongenerated

--- nongenerated/index.js
+++ nongenerated/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
@@ -10,32 +12,38 @@ const app = (0, express_1.default)();
 const port = 3000;
 app.use(express_1.default.json());
 let users = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Doe' }
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Doe" },
 ];
-app.get('/users', (req, res) => {
-    const { name } = req.query;
-    if (name) {
-        //@ts-ignore
-        const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
-        res.json(filteredUsers);
-    }
-    else {
-        res.json(users);
-    }
+app.get("/users", (req, res) => {
+  const { name } = req.query;
+  if (name) {
+    //@ts-ignore
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase())
+    );
+    res.json(filteredUsers);
+  } else {
+    res.json(users);
+  }
 });
-app.get('/post', (req, res) => {
-    const { name } = req.query;
-    if (name) {
-        //@ts-ignore
-        const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
-        res.json(filteredUsers);
-    }
-    else {
-        res.json(users);
-    }
+app.get("/post", (req, res) => {
+  const { name } = req.query;
+  if (name) {
+    //@ts-ignore
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(name.toLowerCase())
+    );
+    res.json(filteredUsers);
+  } else {
+    res.json(users);
+  }
 });
-app.use('/documentation', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(openapispec_1.openapiSpec));
+app.use(
+  "/documentation",
+  swagger_ui_express_1.default.serve,
+  swagger_ui_express_1.default.setup(openapispec_1.openapiSpec)
+);
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
